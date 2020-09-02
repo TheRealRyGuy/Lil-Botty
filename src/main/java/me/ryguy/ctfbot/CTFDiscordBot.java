972bdcd2ef0be8fa9e8ce17cf8be639e85094911
RyGuy <oboylery@gmail.com>
2020-09-02 05:55:30 -0400

    qol map file redfinition

--- src/main/java/me/ryguy/ctfbot/CTFDiscordBot.java
+++ src/main/java/me/ryguy/ctfbot/CTFDiscordBot.java
@@ -9,6 +9,7 @@ import me.ryguy.ctfbot.cmds.SetRolesCommand;
 import me.ryguy.ctfbot.listeners.MemeChatListeners;
 import me.ryguy.ctfbot.temp.BasicEventReactionsListener;
 import me.ryguy.discordapi.DiscordBot;
+import org.apache.commons.lang3.SystemUtils;
 
 import java.io.File;
 import java.util.Arrays;
@@ -22,7 +23,14 @@ public class CTFDiscordBot {
     public static final long SIGNUPS_CHANNEL = 483993420189138975L;
 
     public static final Gson GSON = new Gson();
-    public static final File MAP_FILE = new File(System.getProperty("user.dir") + "/src/main/resources/maps.json");
+    public static File MAP_FILE;
+    static {
+        if(SystemUtils.IS_OS_WINDOWS) {
+            MAP_FILE = new File(System.getProperty("user.dir") + "/src/main/resources/maps.json");
+        }else {
+            MAP_FILE = new File("/Shared/maps.json");
+        }
+    }
 
     public static final List<String> ROLES_TO_REMOVE = Arrays.asList("Red Team", "Blue Team", "playing");
 

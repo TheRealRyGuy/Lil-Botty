703167bfeecffc0d648c88819d044dce9a12cf2a
RyGuy <oboylery@gmail.com>
2020-09-02 17:00:59 -0400

    pom even though my packaging is awful, clean up invite command

--- src/main/java/me/ryguy/ctfbot/cmds/InviteCommand.java
+++ src/main/java/me/ryguy/ctfbot/cmds/InviteCommand.java
@@ -11,9 +11,9 @@ public class InviteCommand extends Command {
 
     @Override
     public Mono<Void> execute(Message message, String s, String[] strings) {
-        message.getChannel().block().createMessage("Invite me to your discord through https://discord.com/oauth2/authorize?client_id=749700983105257482&scope=bot&permissions=2146958847! \n \n " +
+        message.getChannel().block().createMessage("Invite me to your discord through <https://discord.com/oauth2/authorize?client_id=749700983105257482&scope=bot&permissions=2146958847>! \n \n " +
                 "Keep in mind, the bot is still heavily in beta, so expect bugs / missing features \n " +
-                "Feel free to contribute at https://github.com/TheRealRyGuy/CTF-Community-Discord-Bot \n And message RyGuy#0001 with any bugs!").block();
+                "Feel free to contribute at <https://github.com/TheRealRyGuy/CTF-Community-Discord-Bot>").block();
         return null;
     }
 }

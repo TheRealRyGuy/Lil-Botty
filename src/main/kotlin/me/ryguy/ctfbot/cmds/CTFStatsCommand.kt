77a902ec6bc3dd07a71f8925a11e2691f12f411b
NineOneFive <nuevounocinco@gmail.com>
2020-09-11 15:41:19 -0400

    Added leaderboards link

--- src/main/kotlin/me/ryguy/ctfbot/cmds/CTFStatsCommand.kt
+++ src/main/kotlin/me/ryguy/ctfbot/cmds/CTFStatsCommand.kt
@@ -1,6 +1,7 @@
 package me.ryguy.ctfbot.cmds
 
 import discord4j.core.`object`.entity.Message
+import me.ryguy.ctfbot.replyWithSuccess
 import me.ryguy.discordapi.command.Command
 import reactor.core.publisher.Mono
 
@@ -12,10 +13,23 @@ class CTFStatsCommand : Command("ctfstats") {
         val CTFClasses = listOf("Archer", "Assassin", "Chemist", "Dwarf", "Elf", "Engineer",
                 "Fashionista", "Heavy", "Mage", "Medic", "Necro", "Ninja", "Pyro", "Scout", "Soldier"
         )
+
+        const val GITHUB_LINK = "https://nineonefive.github.io/CTFTeamStats/leaderboards.html"
     }
 
     override fun execute(message: Message?, alias: String?, args: Array<out String>?): Mono<Void> {
-        // TODO: retrieve from ctf stats repo
+        // TODO: actual display later
+        message?.channel?.block()?.createEmbed {
+            it.setTitle("CTF Leaderboards")
+            it.setDescription(
+                    """
+                        Other features coming soon:tm:..
+                        
+                        :link: $GITHUB_LINK
+                    """.trimIndent()
+            )
+            it.setFooter("Last updated 2020-09-11", null)
+        }
         return Mono.empty()
     }
 }
